49a63a779e96fc5c29d5b34cd6907fe5d206d018
Aicha ABDEL ALI <67974059+aichaabdelali@users.noreply.github.com>
2020-10-29 21:37:42 +0100

    Update PlaceOrderScreen.js

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -33,7 +33,8 @@ const PlaceOrderScreen = ({ history }) => {
     if (success) {
       history.push(`/order/${order._id}`);
     }
-  }, [history, success, order._id]);
+    // eslint-disable-next-line
+  }, [history, success]);
 
   const placeOrderHandler = () => {
     dispatch(
